68e7f6cda8fadf21f6a9df2e5ffa607934018ed8
sergiofah <fahsergio@gmail.com>
2024-02-02 17:09:00 -0300

    Initial commit: - Base Spring Initializr Project + application.properties (postgres local db integration)

---
+++ src/main/java/com/sergiofah/mssecurityapi/MsSecurityApiApplication.java
@@ -0,0 +1,13 @@
+package com.sergiofah.mssecurityapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsSecurityApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsSecurityApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sergiofah/mssecurityapi/MsSecurityApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sergiofah.mssecurityapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsSecurityApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
