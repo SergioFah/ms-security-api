d62ac64c135a9080f5c0321a4d1581b030c58b92
sergiofah <fahsergio@gmail.com>
2024-02-02 22:40:35 -0300

    - Scheduler added

--- src/main/java/com/sergiofah/mssecurityapi/controller/ValueController.java
+++ src/main/java/com/sergiofah/mssecurityapi/controller/ValueController.java
@@ -8,10 +8,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-import java.util.Optional;
-
-
 @RestController
 @Data
 @RequestMapping("/updates")
